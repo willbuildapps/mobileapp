21409d5951a6f7b2e92bbeecd53f838360061b59
daividssilverio <daivid.silverio@gmail.com>
2018-11-05 19:28:54 -0300

    :construction: Add missing initial calendar shortcuts ui rxfactoring parts

---
+++ Toggl.Giskard/ViewHolders/ReportsCalendarShortcutCellViewHolder.cs
@@ -0,0 +1,40 @@
+using System;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Toggl.Foundation.MvvmCross.ViewModels.ReportsCalendar.QuickSelectShortcuts;
+
+namespace Toggl.Giskard.ViewHolders
+{
+    public class ReportsCalendarShortcutCellViewHolder : BaseRecyclerViewHolder<ReportsCalendarBaseQuickSelectShortcut>
+    {
+        public static ReportsCalendarShortcutCellViewHolder Create(View itemView)
+            => new ReportsCalendarShortcutCellViewHolder(itemView);
+
+        private TextView shortcutText;
+        private GradientDrawable backgroundDrawable;
+
+        public ReportsCalendarShortcutCellViewHolder(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public ReportsCalendarShortcutCellViewHolder(View itemView) : base(itemView)
+        {
+        }
+
+        protected override void InitializeViews()
+        {
+            shortcutText = ItemView as TextView;
+            backgroundDrawable = shortcutText?.Background as GradientDrawable;
+        }
+
+        protected override void UpdateView()
+        {
+            shortcutText.Text = Item.Title;
+            backgroundDrawable.SetColor(Item.Selected ? Color.ParseColor("#328fff") : Color.ParseColor("#3e3e3e"));
+            backgroundDrawable.InvalidateSelf();
+        }
+    }
+}
