c59d4d97e55a8a3664ceaf7182d543bd69415c83
daividssilverio <daivid.silverio@gmail.com>
2018-11-05 19:18:03 -0300

    :wrench: Add presenter handling for reports calendar vm

--- Toggl.Giskard/Presenters/TogglPresenter.cs
+++ Toggl.Giskard/Presenters/TogglPresenter.cs
@@ -57,6 +57,15 @@ namespace Toggl.Giskard.Presenters
             base.ChangePresentation(hint);
         }
 
+        public override void Show(MvxViewModelRequest request)
+        {
+            if (request.ViewModelType == typeof(ReportsCalendarViewModel))
+            {
+                return;
+            }
+            base.Show(request);
+        }
+
         protected override bool CloseFragmentDialog(IMvxViewModel viewModel, MvxDialogFragmentPresentationAttribute attribute)
         {
             var tag = FragmentJavaName(attribute.ViewType);
