dc6af204f2b02c1ee8c96b102ed295f61ac966bf
kornelijepetak <kornelije@gmail.com>
2018-12-18 13:55:08 +0100

    :construction: Update activity to work with the new UI and the VM.

--- Toggl.Giskard/Activities/LoginActivity.ViewBinds.cs
+++ Toggl.Giskard/Activities/LoginActivity.ViewBinds.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Android.Support.Constraints;
+using Android.Support.Design.Widget;
 using Android.Views;
 using Android.Widget;
 
@@ -6,29 +8,55 @@ namespace Toggl.Giskard.Activities
 {
     public sealed partial class LoginActivity
     {
+        private Group pageOneGroup;
+        private Group pageTwoGroup;
+        private Group loggingInGroup;
+
+        private ImageView backButton;
+
+        private TextInputEditText secondScreenEmailTextField;
+        private TextInputEditText passwordTextField;
+        private TextInputEditText loginWithEmailTextField;
+
+        private TextInputLayout secondScreenEmailLayout;
+
+        private TextView loginWithEmailErrorLabel;
+        private TextView secondScreenErrorLabel;
+        private TextView needHelpContactUsButton;
+        private TextView secondScreenEmailLabel;
+
+        private ProgressBar activityIndicator;
+
+        private Button loginWithEmailButton;
+        private Button googleLoginButton;
+        private TextView forgotPasswordButton;
         private Button loginButton;
-
-        private View signupCard;
-        private View googleLoginButton;
-        private View forgotPasswordView;
-
-        private EditText emailEditText;
-        private EditText passwordEditText;
-
-        private TextView errorTextView;
-
-        private ProgressBar progressBar;
 
         protected override void InitializeViews()
         {
-            signupCard = FindViewById(Resource.Id.LoginSignupCardView);
-            errorTextView = FindViewById<TextView>(Resource.Id.LoginError);
-            loginButton = FindViewById<Button>(Resource.Id.LoginLoginButton);
-            forgotPasswordView = FindViewById(Resource.Id.LoginForgotPassword);
-            googleLoginButton = FindViewById<View>(Resource.Id.LoginGoogleLogin);
-            progressBar = FindViewById<ProgressBar>(Resource.Id.LoginProgressBar);
-            emailEditText = FindViewById<EditText>(Resource.Id.LoginEmailEditText);
-            passwordEditText = FindViewById<EditText>(Resource.Id.LoginPasswordEditText);
+            pageOneGroup = FindViewById<Group>(Resource.Id.PageOneGroup);
+            pageTwoGroup = FindViewById<Group>(Resource.Id.PageTwoGroup);
+            loggingInGroup = FindViewById<Group>(Resource.Id.LoggingInGroup);
+
+            backButton = FindViewById<ImageView>(Resource.Id.BackButton);
+
+            secondScreenEmailTextField = FindViewById<TextInputEditText>(Resource.Id.SecondScreenEmailTextField);
+            passwordTextField = FindViewById<TextInputEditText>(Resource.Id.PasswordTextField);
+            loginWithEmailTextField = FindViewById<TextInputEditText>(Resource.Id.LoginWithEmailTextField);
+
+            secondScreenEmailLayout = FindViewById<TextInputLayout>(Resource.Id.SecondScreenEmailLayout);
+
+            loginWithEmailErrorLabel = FindViewById<TextView>(Resource.Id.LoginWithEmailErrorLabel);
+            secondScreenErrorLabel = FindViewById<TextView>(Resource.Id.SecondScreenErrorLabel);
+            needHelpContactUsButton = FindViewById<TextView>(Resource.Id.NeedHelpContactUsButton);
+            secondScreenEmailLabel = FindViewById<TextView>(Resource.Id.SecondScreenEmailLabel);
+
+            activityIndicator = FindViewById<ProgressBar>(Resource.Id.ActivityIndicator);
+
+            loginWithEmailButton = FindViewById<Button>(Resource.Id.LoginWithEmailButton);
+            googleLoginButton = FindViewById<Button>(Resource.Id.GoogleLoginButton);
+            forgotPasswordButton = FindViewById<TextView>(Resource.Id.ForgotPasswordButton);
+            loginButton = FindViewById<Button>(Resource.Id.LoginButton);
         }
     }
 }
--- Toggl.Giskard/Activities/LoginActivity.cs
+++ Toggl.Giskard/Activities/LoginActivity.cs
@@ -2,8 +2,10 @@ using System;
 using System.Reactive.Linq;
 using Android.App;
 using Android.Content.PM;
+using Android.Graphics;
 using Android.OS;
 using Android.Views;
+using Android.Widget;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using Toggl.Foundation.MvvmCross.Extensions;
 using Toggl.Foundation.MvvmCross.ViewModels;
@@ -17,17 +19,178 @@ namespace Toggl.Giskard.Activities
     [MvxActivityPresentation]
     [Activity(Theme = "@style/AppTheme.WhiteStatusBar",
               ScreenOrientation = ScreenOrientation.Portrait,
-              WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden,
+              WindowSoftInputMode = SoftInput.AdjustResize,
               ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
     public sealed partial class LoginActivity : ReactiveActivity<LoginViewModel>
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+
+            this.ChangeStatusBarColor(Color.White, true);
             SetContentView(Resource.Layout.LoginActivity);
             OverridePendingTransition(Resource.Animation.abc_slide_in_bottom, Resource.Animation.abc_fade_out);
 
             InitializeViews();
+
+            setupBindings();
+
+            loginWithEmailTextField.SetFocus();
         }
+
+        private void setupBindings()
+        {
+            backButton.Rx()
+                .BindAction(ViewModel.Back)
+                .DisposedBy(DisposeBag);
+
+            loginWithEmailTextField.Rx().Text()
+                .Subscribe(ViewModel.EmailRelay.Accept)
+                .DisposedBy(DisposeBag);
+
+            loginWithEmailTextField.Rx().EditorActionSent()
+                .SelectUnit()
+                .Subscribe(ViewModel.LoginWithEmail.Inputs)
+                .DisposedBy(DisposeBag);
+
+            loginWithEmailButton.Rx()
+                .BindAction(ViewModel.LoginWithEmail)
+                .DisposedBy(DisposeBag);
+
+            googleLoginButton.Rx()
+                .BindAction(ViewModel.LoginWithGoogle)
+                .DisposedBy(DisposeBag);
+
+            ViewModel.LoginWithEmail.Errors
+                .Select(e => e.Message)
+                .Subscribe(loginWithEmailErrorLabel.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.LoginWithGoogle.Errors
+                .Select(e => e.Message)
+                .Subscribe(loginWithEmailErrorLabel.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.LoginWithEmail.Errors
+                .SelectValue(true)
+                .Subscribe(loginWithEmailErrorLabel.Rx().IsVisible())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.ClearEmailScreenError
+                .SelectValue(string.Empty)
+                .Subscribe(loginWithEmailErrorLabel.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+
+            // second screen
+            forgotPasswordButton.Rx()
+                .BindAction(ViewModel.ForgotPassword)
+                .DisposedBy(DisposeBag);
+
+            loginButton.Rx()
+                .BindAction(ViewModel.Login)
+                .DisposedBy(DisposeBag);
+
+            passwordTextField.Rx().EditorActionSent()
+                .SelectUnit()
+                .Subscribe(ViewModel.Login.Inputs)
+                .DisposedBy(DisposeBag);
+
+            secondScreenEmailTextField.Rx().EditorActionSent()
+                .Subscribe(_ => passwordTextField.SetFocus())
+                .DisposedBy(DisposeBag);
+
+            secondScreenEmailTextField.Rx().Text()
+                .Subscribe(ViewModel.EmailRelay.Accept)
+                .DisposedBy(DisposeBag);
+
+            needHelpContactUsButton.Rx()
+                .BindAction(ViewModel.ContactUs)
+                .DisposedBy(DisposeBag);
+
+            ViewModel.EmailRelay
+                .Subscribe(secondScreenEmailTextField.Rx().TextObserver(ignoreUnchanged: true))
+                .DisposedBy(DisposeBag);
+
+            ViewModel.EmailRelay
+                .Subscribe(secondScreenEmailLabel.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.IsEmailFieldEdittable
+                .Subscribe(secondScreenEmailLayout.Rx().IsVisible(false))
+                .DisposedBy(DisposeBag);
+
+            ViewModel.IsEmailFieldEdittable
+                .Invert()
+                .Subscribe(loggingInGroup.Rx().IsVisible())
+                .DisposedBy(DisposeBag);
+
+            passwordTextField.Rx().Text()
+                .Subscribe(ViewModel.PasswordRelay.Accept)
+                .DisposedBy(DisposeBag);
+
+            ViewModel.SuggestContactSupport
+                .Subscribe(needHelpContactUsButton.Rx().IsVisible())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.ClearPasswordScreenError
+                .SelectValue(string.Empty)
+                .Subscribe(secondScreenErrorLabel.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.Login.Errors
+                .Select(e => e.Message)
+                .Subscribe(secondScreenErrorLabel.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.Login.Errors
+                .SelectValue(true)
+                .Subscribe(secondScreenErrorLabel.Rx().IsVisible())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.PasswordRelay
+                .Subscribe(passwordTextField.Rx().TextObserver(ignoreUnchanged: true))
+                .DisposedBy(DisposeBag);
+
+            ViewModel.IsLoggingIn
+                .Subscribe(activityIndicator.Rx().IsVisible())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.IsLoggingIn
+                .Select(loginButtonTitle)
+                .Subscribe(loginButton.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.IsInSecondScreen
+                .Invert()
+                .Do(setFocus(loginWithEmailTextField))
+                .Subscribe(pageOneGroup.Rx().IsVisible())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.IsInSecondScreen
+                .Invert()
+                .Subscribe(loginWithEmailTextField.Rx().IsVisible())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.IsInSecondScreen
+                .Do(setFocus(passwordTextField))
+                .Subscribe(pageTwoGroup.Rx().IsVisible())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.IsInSecondScreen
+               .Subscribe(loggingInGroup.Rx().IsVisible())
+               .DisposedBy(DisposeBag);
+        }
+
+        private Action<bool> setFocus(EditText editText)
+        {
+            return shouldFocus =>
+            {
+                if (shouldFocus)
+                    editText.SetFocus();
+            };
+        }
+
+        private string loginButtonTitle(bool isLoading)
+            => isLoading ? "" : GetString(Resource.String.Login);
     }
 }
