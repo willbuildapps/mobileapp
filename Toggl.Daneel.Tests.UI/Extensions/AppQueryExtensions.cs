ce8327f0e2ca5b81eb33cd62ea2e1e15728d1efb
daividssilverio <daivid.silverio@gmail.com>
2018-12-21 18:56:40 -0300

    :construction: Update iOS WaitForElementWithText to include text inside child views

--- Toggl.Daneel.Tests.UI/Extensions/AppQueryExtensions.cs
+++ Toggl.Daneel.Tests.UI/Extensions/AppQueryExtensions.cs
@@ -1,4 +1,7 @@
+using System.Linq;
+using NUnit.Framework;
 using Xamarin.UITest;
+
 
 namespace Toggl.Tests.UI.Extensions
 {
@@ -6,7 +9,14 @@ namespace Toggl.Tests.UI.Extensions
     {
         public static void WaitForElementWithText(this IApp app, string element, string text)
         {
-            app.WaitForElement(x => x.Marked(element).Text(text));
+            app.WaitFor(() => app.findElementWithText(element, text), $"Could not find element {element} with text {text}");
+            Assert.IsTrue(app.findElementWithText(element, text));
+        }
+
+        private static bool findElementWithText(this IApp app, string element, string text)
+        {
+            return app.Query(x => x.Marked(element).Text(text)).FirstOrDefault() != null
+                   || app.Query(x => x.Marked(element).Descendant().Text(text)).FirstOrDefault() != null;
         }
     }
 }
