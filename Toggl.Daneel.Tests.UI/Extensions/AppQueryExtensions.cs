980344960ce152347375abbddba21b60ba53e6dd
daividssilverio <daivid.silverio@gmail.com>
2018-12-31 11:54:58 -0300

    :sparkles: Add NavigateBack UITest extension

--- Toggl.Daneel.Tests.UI/Extensions/AppQueryExtensions.cs
+++ Toggl.Daneel.Tests.UI/Extensions/AppQueryExtensions.cs
@@ -7,6 +7,21 @@ namespace Toggl.Tests.UI.Extensions
 {
     public static class AppQueryExtensions
     {
+        private static readonly string defaultCloseButtonAccessibilityId = "Close";
+
+        public static void NavigateBack(this IApp app)
+        {
+            var closeButton = app.Query(defaultCloseButtonAccessibilityId).FirstOrDefault();
+            if (closeButton != null)
+            {
+                app.Tap(defaultCloseButtonAccessibilityId);
+            }
+            else
+            {
+                app.Back();
+            }
+        }
+
         public static void WaitForElementWithText(this IApp app, string element, string text)
         {
             app.WaitFor(() => app.findElementWithText(element, text), $"Could not find element {element} with text {text}");
