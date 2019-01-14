4b9a695a2220891fc2913e59be925a7d29836fbb
daividssilverio <daivid.silverio@gmail.com>
2019-01-14 09:48:18 -0300

    :lipstick:

--- Toggl.Giskard/Extensions/PointExtensions.cs
+++ Toggl.Giskard/Extensions/PointExtensions.cs
@@ -8,7 +8,7 @@ namespace Toggl.Giskard.Extensions
         public static PointF ToPointF(this MultivacPoint point)
             => new PointF((float)point.X, (float)point.Y);
 
-        public static MultivacPoint ToMultivacPoint(this PointF point)
+        public static MultivacPoint ToPoint(this PointF point)
             => new MultivacPoint { X = point.X, Y = point.Y };
     }
 }
