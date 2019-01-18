eee29b9ac2c0c9673496262b9bea9cb42534ccb8
daividssilverio <daivid.silverio@gmail.com>
2019-01-18 11:44:04 -0300

    :construction: Add start/end time/date bindings

--- Toggl.Giskard/Activities/EditDurationActivity.ViewBinds.cs
+++ Toggl.Giskard/Activities/EditDurationActivity.ViewBinds.cs
@@ -1,9 +1,38 @@
+using Android.Support.Constraints;
+using Android.Views;
+using Android.Widget;
+
 namespace Toggl.Giskard.Activities
 {
     public partial class EditDurationActivity
     {
+        private View startIcon;
+        private TextView startLabel;
+        private TextView startTimeText;
+        private View startDotSeparator;
+        private TextView startDateText;
+        private View stopIcon;
+        private View stopLabel;
+        private TextView stopTimeText;
+        private TextView stopTimerLabel;
+        private View stopDotSeparator;
+        private TextView stopDateText;
+        private View wheel;
+
         protected override void InitializeViews()
         {
+            startIcon = FindViewById<View>(Resource.Id.StartIcon);
+            startLabel = FindViewById<TextView>(Resource.Id.StartLabel);
+            startTimeText = FindViewById<TextView>(Resource.Id.StartTimeText);
+            startDotSeparator = FindViewById<View>(Resource.Id.StartDotSeparator);
+            startDateText = FindViewById<TextView>(Resource.Id.StartDateText);
+            stopIcon = FindViewById<View>(Resource.Id.StopIcon);
+            stopLabel = FindViewById<View>(Resource.Id.StopLabel);
+            stopTimeText = FindViewById<TextView>(Resource.Id.StopTimeText);
+            stopTimerLabel = FindViewById<TextView>(Resource.Id.StopTimerLabel);
+            stopDotSeparator = FindViewById<View>(Resource.Id.StopDotSeparator);
+            stopDateText = FindViewById<TextView>(Resource.Id.StopDateText);
+            wheel = FindViewById<View>(Resource.Id.Wheel);
         }
     }
 }
--- Toggl.Giskard/Activities/EditDurationActivity.cs
+++ Toggl.Giskard/Activities/EditDurationActivity.cs
@@ -1,9 +1,18 @@
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
+using Android.Widget;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using Toggl.Foundation.MvvmCross.ViewModels;
+using Toggl.Giskard.Extensions;
+using Toggl.Giskard.Extensions.Reactive;
+using Toggl.Multivac.Extensions;
 
 namespace Toggl.Giskard.Activities
 {
@@ -14,10 +23,205 @@ namespace Toggl.Giskard.Activities
         ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
     public sealed partial class EditDurationActivity : ReactiveActivity<EditDurationViewModel>
     {
+        private EditMode editMode;
+        private Dialog editDialog;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.EditDurationActivity);
+            InitializeViews();
+
+            ViewModel.StartTimeString
+                .Subscribe(startTimeText.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.StartDateString
+                .Subscribe(startDateText.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.StopTimeString
+                .Subscribe(stopTimeText.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.StopDateString
+                .Subscribe(stopDateText.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+
+            ViewModel.IsRunning
+                .Subscribe(running =>
+                {
+                    var stopDateTimeViewsVisibility = (!running).ToVisibility(useGone: false);
+                    stopTimerLabel.Visibility = running.ToVisibility();
+                    stopTimeText.Visibility = stopDateTimeViewsVisibility;
+                    stopDateText.Visibility = stopDateTimeViewsVisibility;
+                    stopDotSeparator.Visibility = stopDateTimeViewsVisibility;
+                })
+                .DisposedBy(DisposeBag);
+
+            stopTimerLabel.Rx()
+                .BindAction(ViewModel.EditStopTime)
+                .DisposedBy(DisposeBag);
+
+            stopTimerLabel.Rx().Tap()
+                .Subscribe(_ => { editMode = EditMode.Time; })
+                .DisposedBy(DisposeBag);
+
+            startTimeText.Rx().Tap()
+                .Subscribe(_ => { editMode = EditMode.Time; })
+                .DisposedBy(DisposeBag);
+
+            startDateText.Rx().Tap()
+                .Subscribe(_ => { editMode = EditMode.Date; })
+                .DisposedBy(DisposeBag);
+
+            startTimeText.Rx()
+                .BindAction(ViewModel.EditStartTime)
+                .DisposedBy(DisposeBag);
+
+            startDateText.Rx()
+                .BindAction(ViewModel.EditStartTime)
+                .DisposedBy(DisposeBag);
+
+            stopTimeText.Rx().Tap()
+                .Subscribe(_ => { editMode = EditMode.Time; })
+                .DisposedBy(DisposeBag);
+
+            stopDateText.Rx().Tap()
+                .Subscribe(_ => { editMode = EditMode.Date; })
+                .DisposedBy(DisposeBag);
+
+            stopTimeText.Rx()
+                .BindAction(ViewModel.EditStopTime)
+                .DisposedBy(DisposeBag);
+
+            stopDateText.Rx()
+                .BindAction(ViewModel.EditStopTime)
+                .DisposedBy(DisposeBag);
+
+
+            stopTimeText.Rx().Tap()
+                .Subscribe()
+                .DisposedBy(DisposeBag);
+
+            stopDateText.Rx().Tap()
+                .Subscribe()
+                .DisposedBy(DisposeBag);
+
+            var activeTimeSubject = new Subject<DateTimeOffset>();
+            var editEndedSubject = new Subject<Unit>();
+
+            ViewModel.IsEditingStartTime
+                .Where(CommonFunctions.Identity)
+                .SelectMany(_ => ViewModel.StartTime)
+                .Subscribe(time =>
+                {
+                    if (editMode == EditMode.Time)
+                    {
+                        editTime(time.ToLocalTime(), activeTimeSubject, editEndedSubject);
+                    }
+                    else
+                    {
+                        editDate(time.ToLocalTime(), activeTimeSubject, editEndedSubject);
+                    }
+                })
+                .DisposedBy(DisposeBag);
+
+            ViewModel.IsEditingStopTime
+                .Where(CommonFunctions.Identity)
+                .SelectMany(_ => ViewModel.StopTime)
+                .Subscribe(time =>
+                {
+                    if (editMode == EditMode.Time)
+                    {
+                        editTime(time.ToLocalTime(), activeTimeSubject, editEndedSubject);
+                    }
+                    else
+                    {
+                        editDate(time.ToLocalTime(), activeTimeSubject, editEndedSubject);
+                    }
+                })
+                .DisposedBy(DisposeBag);
+
+            activeTimeSubject
+                .Subscribe(ViewModel.ChangeActiveTime.Inputs)
+                .DisposedBy(DisposeBag);
+
+            editEndedSubject
+                .Subscribe(ViewModel.StopEditingTime.Inputs)
+                .DisposedBy(DisposeBag);
+        }
+
+        private void editTime(DateTimeOffset currentTime, Subject<DateTimeOffset> timeChangedSubject, Subject<Unit> editEndedSubject)
+        {
+            if (editDialog == null)
+            {
+                editDialog = new TimePickerDialog(this, new TimePickerListener(currentTime, timeChangedSubject),
+                    currentTime.Hour, currentTime.Minute, false);
+                editDialog.DismissEvent += (_, __) =>
+                {
+                    editDialog = null;
+                    editEndedSubject.OnNext(Unit.Default);
+                };
+                editDialog.Show();
+            }
+        }
+
+        private void editDate(DateTimeOffset currentDate, Subject<DateTimeOffset> dateChangedSubject, Subject<Unit> editEndedSubject)
+        {
+            if (editDialog == null)
+            {
+                editDialog = new DatePickerDialog(this, new DatePickerListener(currentDate, dateChangedSubject),
+                    currentDate.Year, currentDate.Month, currentDate.Day);
+                editDialog.DismissEvent += (_, __) =>
+                {
+                    editDialog = null;
+                    editEndedSubject.OnNext(Unit.Default);
+                };
+                editDialog.Show();
+            }
+        }
+
+        private sealed class TimePickerListener : Java.Lang.Object, TimePickerDialog.IOnTimeSetListener
+        {
+            private readonly DateTimeOffset currentTime;
+            private readonly Subject<DateTimeOffset> editTimeSubject;
+
+            public TimePickerListener(DateTimeOffset currentTime, Subject<DateTimeOffset> editTimeSubject)
+            {
+                this.currentTime = currentTime;
+                this.editTimeSubject = editTimeSubject;
+            }
+
+            public void OnTimeSet(TimePicker view, int hourOfDay, int minute)
+            {
+                var pickedTime = new DateTimeOffset(currentTime.Year, currentTime.Month, currentTime.Day, hourOfDay, minute, currentTime.Minute, currentTime.Millisecond, currentTime.Offset);
+                editTimeSubject.OnNext(pickedTime);
+            }
+        }
+
+        private sealed class DatePickerListener : Java.Lang.Object, DatePickerDialog.IOnDateSetListener
+        {
+            private readonly DateTimeOffset currentDate;
+            private readonly Subject<DateTimeOffset> editDateSubject;
+
+            public DatePickerListener(DateTimeOffset currentDate, Subject<DateTimeOffset> editDateSubject)
+            {
+                this.currentDate = currentDate;
+                this.editDateSubject = editDateSubject;
+            }
+
+            public void OnDateSet(DatePicker view, int year, int month, int dayOfMonth)
+            {
+                var pickedDate = new DateTimeOffset(year, month, dayOfMonth, currentDate.Hour, currentDate.Minute, currentDate.Minute, currentDate.Millisecond, currentDate.Offset);
+                editDateSubject.OnNext(pickedDate);
+            }
+        }
+
+        private enum EditMode
+        {
+            Time,
+            Date
         }
     }
 }
