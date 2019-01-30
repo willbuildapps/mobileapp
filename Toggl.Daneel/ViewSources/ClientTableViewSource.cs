bbe1d67c43f255de814b37ec8821f2ca8dbf302d
Ricardo Sanchez <odrakir@gmail.com>
2019-01-30 12:10:09 +0100

    Extract cell configuration for ClientTableView

--- Toggl.Daneel/ViewSources/ClientTableViewSource.cs
+++ Toggl.Daneel/ViewSources/ClientTableViewSource.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
-using System.Threading.Tasks;
 using Foundation;
 using Toggl.Daneel.Cells;
 using Toggl.Daneel.Views.Client;
-using Toggl.Foundation.MvvmCross.Collections;
 using Toggl.Foundation.MvvmCross.ViewModels;
 using UIKit;
 
 namespace Toggl.Daneel.ViewSources
 {
-    public sealed class ClientTableViewSource : ListTableViewSource<SelectableClientBaseViewModel, ClientViewCell>
+    public sealed class ClientTableViewSource : ListTableViewSource<SelectableClientBaseViewModel>
     {
         public IObservable<SelectableClientBaseViewModel> ClientSelected
             => Observable
@@ -22,9 +19,9 @@ namespace Toggl.Daneel.ViewSources
 
         private const int rowHeight = 48;
 
-        public ClientTableViewSource()
-            : base(ImmutableArray<SelectableClientBaseViewModel>.Empty, ClientViewCell.Identifier)
+        public ClientTableViewSource() : base(ImmutableArray<SelectableClientBaseViewModel>.Empty)
         {
+            ConfigureCell = configureCell;
         }
 
         public void SetNewClients(IEnumerable<SelectableClientBaseViewModel> clients)
@@ -32,15 +29,15 @@ namespace Toggl.Daneel.ViewSources
             items = clients.ToImmutableList();
         }
 
-        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        public override nfloat GetHeightForRow(UITableView tableView, NSIndexPath indexPath) => rowHeight;
+
+        private UITableViewCell configureCell(ListTableViewSource<SelectableClientBaseViewModel> source,
+            UITableView tableView, NSIndexPath indexPath, SelectableClientBaseViewModel model)
         {
-            var item = items[indexPath.Row];
-            var identifier = item is SelectableClientCreationViewModel ? CreateClientViewCell.Identifier : cellIdentifier;
+            var identifier = model is SelectableClientCreationViewModel ? CreateClientViewCell.Identifier : ClientViewCell.Identifier;
             var cell = tableView.DequeueReusableCell(identifier) as BaseTableViewCell<SelectableClientBaseViewModel>;
-            cell.Item = item;
+            cell.Item = model;
             return cell;
         }
-
-        public override nfloat GetHeightForRow(UITableView tableView, NSIndexPath indexPath) => rowHeight;
     }
 }
